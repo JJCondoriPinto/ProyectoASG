be2211a8b6d7b6f10a84736c7113c78c6177615e
Abdieldixon22 <107158578+Abdieldixon22@users.noreply.github.com>
2022-12-07 19:22:36 -0600

    Delete ProyectoPOO_Sis_Security directory

--- ProyectoPOO_Sis_Security/src/Modelo/PermisosDAO.java
+++
@@ -1,6 +0,0 @@
-package Modelo;
-public class PermisosDAO {
-    private final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
-    private final String USER = "GESTION_ASG";
-    private final String PASS = "oracle123";
-}
